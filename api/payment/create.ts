ecf2761599517d0528cf5e8aac4a73a660d0a20c
Ereklebazaovi <your-email@example.com>
2025-12-25 00:19:43 +0400

    tbc payment with vercel serverless

--- api/payment/create.ts
+++ api/payment/create.ts
@@ -15,7 +15,7 @@ export default async function handler(req: VercelRequest, res: VercelResponse) {
 
   if (req.method === 'OPTIONS') return res.status(200).end();
   if (req.method !== "POST") return res.status(405).json({ error: "Method not allowed" });
-
+//
   try {
     const { orderId, amount, description } = req.body; // email ამოვიღეთ აქედანაც
 
