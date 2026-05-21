0e62a081172d70d740d3d8fdacf980b5dacb66af
Ereklebazanovi <erekle.bazanovi.1@iliauni.edu.ge>
2026-05-21 22:52:56 +0400

    chore: remove migration secret after slug migration

--- api/order/getByNumber.ts
+++ api/order/getByNumber.ts
@@ -2,7 +2,7 @@ import type { VercelRequest, VercelResponse } from "@vercel/node";
 import { adminDb } from "../lib/firebase-admin";
 
 export default async function handler(req: VercelRequest, res: VercelResponse) {
-  // CORS
+  // CORSSS
   res.setHeader('Access-Control-Allow-Credentials', 'true');
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS');
